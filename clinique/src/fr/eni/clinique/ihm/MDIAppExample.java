372ef6429628fceff132f55fe4253d73655603ef
DarkXelus <DarkXelus@users.noreply.github.com>
2018-06-25 12:04:21 +0200

    Test 1

--- clinique/src/fr/eni/clinique/ihm/MDIAppExample.java
+++ clinique/src/fr/eni/clinique/ihm/MDIAppExample.java
@@ -21,7 +21,6 @@ public class MDIAppExample extends JFrame implements ActionListener {
 	private JMenuBar menuBarre;
 	private JMenu menuAgenda;
 	private InternalFrame1 frm1;
-	private JMenu test;
 
 
 	public MDIAppExample() {

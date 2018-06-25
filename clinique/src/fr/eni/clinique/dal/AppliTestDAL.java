0bfe3d976d2d05e2851d803db5c90a72836c4931
DarkXelus <DarkXelus@users.noreply.github.com>
2018-06-25 16:26:22 +0200

    BLLTest

---
+++ clinique/src/fr/eni/clinique/dal/AppliTestDAL.java
@@ -0,0 +1,34 @@
+package fr.eni.clinique.dal;
+
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import fr.eni.clinique.dal.jdbc.ConnexionDAOJdbcImpl;
+
+public class AppliTestDAL {
+
+	public static void main(String[] args) throws DALException {
+
+		Connection con = null;
+		PreparedStatement rqt = null;
+		ResultSet rs = null;
+
+		try {
+			con = JdbcTools.getConnection();
+
+			rqt = con.prepareStatement("select * from Clients");
+			rs = rqt.executeQuery();
+
+			if (rs.next()) {
+				System.out.println(rs.getString("NomClient"));
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

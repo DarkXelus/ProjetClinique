27a5d24a1f211f5e0596509401868d1ebfbc1d82
DarkXelus <DarkXelus@users.noreply.github.com>
2018-06-26 16:49:17 +0200

    LastCommitDay2

--- clinique/src/fr/eni/clinique/bll/AppliTestBll.java
+++ clinique/src/fr/eni/clinique/bll/AppliTestBll.java
@@ -7,28 +7,35 @@ import fr.eni.clinique.BO.Personnels;
 
 public class AppliTestBll {
 	public static void main(String[] args) {
-		
+
 		LoginManager lm = new LoginManager();
-		String test = lm.Login("BOSAPIN","AniForm");
-		System.out.println(test);
+		String test;
+		try {
+			test = lm.Login("BOSAPIN", "AniForm");
+			System.out.println(test);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 
 		PersonnelsManager pm = new PersonnelsManager();
 		List<Personnels> lstPersonnels = new ArrayList<Personnels>();
-		
+
 		Personnels perso;
 		try {
-			perso = new Personnels("TOURNE","AniForm","VET",false);
+			perso = new Personnels("TOURNE", "AniForm", "VET", false);
 			pm.Create(perso);
 			lstPersonnels = pm.SelectAll();
-			
+
 			for (Personnels personnels : lstPersonnels) {
 				System.out.println(personnels.toString());
 			}
-			
+
 		} catch (BLLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }

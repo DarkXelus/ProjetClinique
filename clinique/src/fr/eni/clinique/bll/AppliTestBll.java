8e76145631166f5ca91a7a777ed363b63389e209
theotourgis <th.tourgis@hotmail.com>
2018-06-27 09:46:21 +0200

    IHM

--- clinique/src/fr/eni/clinique/bll/AppliTestBll.java
+++ clinique/src/fr/eni/clinique/bll/AppliTestBll.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.clinique.BO.Personnels;
+import fr.eni.clinique.dal.DALException;
 
 public class AppliTestBll {
 	public static void main(String[] args) {
@@ -11,21 +12,12 @@ public class AppliTestBll {
 		LoginManager lm = new LoginManager();
 		String test;
 		try {
-<<<<<<< HEAD
-			test = lm.Login("BOSAPIN","AniForm");
-		System.out.println(test);
-=======
 			test = lm.Login("BOSAPIN", "AniForm");
 			System.out.println(test);
->>>>>>> 27a5d24a1f211f5e0596509401868d1ebfbc1d82
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-<<<<<<< HEAD
-=======
-		
->>>>>>> 27a5d24a1f211f5e0596509401868d1ebfbc1d82
 
 		PersonnelsManager pm = new PersonnelsManager();
 		List<Personnels> lstPersonnels = new ArrayList<Personnels>();
@@ -33,14 +25,16 @@ public class AppliTestBll {
 		Personnels perso;
 		try {
 			perso = new Personnels("TOURNE", "AniForm", "VET", false);
-			pm.Create(perso);
-			lstPersonnels = pm.SelectAll();
+			
+				pm.Create(perso);
 
+				lstPersonnels = pm.SelectAll();
+			
 			for (Personnels personnels : lstPersonnels) {
 				System.out.println(personnels.toString());
 			}
 
-		} catch (BLLException e) {
+		} catch (DALException | BLLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
